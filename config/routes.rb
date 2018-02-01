d96f00fc3cd250b5a6871e5d20aeddf95838fa3f
Shih_Kuan <cliff1304@gmail.com>
2018-02-01 23:32:17 +0800

    Update routes rule for ToDoList.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/users", to: "users#index"
-  get "/", to: "users#index"
+  get "/", to: "tasks#index"
+  resources :tasks do
+    member do
+      post :done
+    end
+  end
 end
